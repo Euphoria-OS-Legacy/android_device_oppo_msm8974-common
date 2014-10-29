bb43018a893df74d5f1385870414145258db604d
Michael Bestas <mikeioannina@gmail.com>
2014-10-29 23:27:32 +0200

    msm8974: Move aapt config to devices
    Change-Id: Ia205ce3e7ccf7b24bbe965f3994dfd3d88428002

--- msm8974.mk
+++ msm8974.mk
@@ -259,10 +259,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
 #    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml \
 
-# Device uses high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
-
 # call the proprietary setup
 $(call inherit-product-if-exists, vendor/oppo/msm8974-common/msm8974-common-vendor.mk)
 
